21b4bfcb1f6ac76dd9246f0bd896c1023dacf82c
Scott Fox <scottfox87@gmail.com>
2018-01-10 19:48:57 -0600

    initialize project and sketch out pseudocode

---
+++ lib/hangman.rb
@@ -0,0 +1,38 @@
+puts "Hangman initialized!"
+
+
+# load in dictionary
+# load_dictionary
+
+
+# randomly select a word between 5 and 12 characters
+# select_word_from_dictionary
+
+
+
+# display some sort of count so the player knows how many more incorrect guesses she has before the game ends
+# calculate_guess_count
+# display_guess_count
+
+# display correct chosen letters in their respective places
+# display_right_choices
+
+
+# display list of incorrectly chosen letters
+# display_wrong_choices
+
+
+
+# Now implement the functionality where, at the start of any turn, instead of making a guess the player should
+# also have the option to save the game. Remember what you learned about serializing objects… you can serialize your game class too!
+# give_option_to_save
+# save_game
+
+
+# When the program first loads, add in an option that allows you to open one of your saved games,
+# which should jump you exactly back to where you were when you saved. Play on!
+# give_option_to_load_saved_game
+# load_saved_game
+
+
+
